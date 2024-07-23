82f0f6574eaddae06271f46d51fec229e310ea02
Vicente Marti Soriano <vicentemline@gmail.com>
2024-07-23 21:13:52 +0200

    primer commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,57 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
+import { CreateProductDto, UpdateProductDto } from './dto';
+
+@Controller('products')
+export class ProductsController {
+    constructor(@Inject(PRODUCT_SERVICE) private readonly products_client: ClientProxy) {}
+
+    @Post()
+    createProduct(@Body() createProductDto: CreateProductDto) {
+        return this.products_client.send({ cmd: 'create_product' }, createProductDto).pipe(
+            catchError((err) => {
+                throw new RpcException(err);
+            }),
+        );
+    }
+
+    @Get()
+    findAllProducts(@Query() paginationDto: PaginationDto) {
+        return this.products_client.send({ cmd: 'find_all_products' }, paginationDto).pipe(
+            catchError((err) => {
+                throw new RpcException(err);
+            }),
+        );
+    }
+
+    @Get(':id')
+    async findOneProduct(@Param('id', ParseIntPipe) id: number) {
+        return this.products_client.send({ cmd: 'find_one_product' }, { id }).pipe(
+            catchError((err) => {
+                throw new RpcException(err);
+            }),
+        );
+    }
+
+    @Delete(':id')
+    deleteProduct(@Param('id', ParseIntPipe) id: number) {
+        return this.products_client.send({ cmd: 'delete_product' }, { id }).pipe(
+            catchError((err) => {
+                throw new RpcException(err);
+            }),
+        );
+    }
+
+    @Patch(':id')
+    updateProduct(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
+        return this.products_client.send({ cmd: 'update_product' }, { id, ...updateProductDto }).pipe(
+            catchError((err) => {
+                throw new RpcException(err);
+            }),
+        );
+    }
+}
