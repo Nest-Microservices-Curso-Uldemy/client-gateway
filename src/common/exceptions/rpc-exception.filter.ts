cdcf0152475533d84e778c77deb9d0dc94175fa4
Vicente Marti Soriano <vicentemline@gmail.com>
2024-07-30 15:45:40 +0200

    añadiendo docker

--- src/common/exceptions/rpc-exception.filter.ts
+++ src/common/exceptions/rpc-exception.filter.ts
@@ -1,25 +1,25 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable indent */
-import { Catch, ArgumentsHost, ExceptionFilter } from '@nestjs/common';
+import { Catch, ArgumentsHost, ExceptionFilter, HttpStatus } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { Response } from 'express';
 
 @Catch(RpcException)
 export class RpcCustomExceptionFilter implements ExceptionFilter {
-    catch(exception: RpcException, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response: Response = ctx.getResponse();
+	catch(exception: RpcException, host: ArgumentsHost) {
+		const ctx = host.switchToHttp();
+		const response: Response = ctx.getResponse();
 
-        const rpcError = exception.getError();
+		const rpcError = exception.getError();
 
-        if (typeof rpcError === 'object' && 'status' in rpcError && 'message' in rpcError) {
-            const status = isNaN(+rpcError.status) ? 400 : +rpcError.status;
-            return response.status(status).json(rpcError);
-        }
+		if (rpcError.toString().includes('Empty response')) {
+			const message = rpcError.toString().substring(0, rpcError.toString().indexOf('(') - 1);
+			return response.status(HttpStatus.BAD_GATEWAY).json({ status: HttpStatus.BAD_GATEWAY, message });
+		}
 
-        response.status(400).json({
-            status: 400,
-            message: rpcError,
-        });
-    }
+		if (typeof rpcError === 'object' && 'status' in rpcError && 'message' in rpcError) {
+			const status = isNaN(+rpcError.status) ? HttpStatus.BAD_REQUEST : +rpcError.status;
+			return response.status(status).json(rpcError);
+		}
+
+		response.status(HttpStatus.BAD_REQUEST).json({ status: HttpStatus.BAD_REQUEST, message: rpcError });
+	}
 }
