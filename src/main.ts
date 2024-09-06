7cb67ab1215cdee4bf023b544430f82c6345601c
Vicente Marti Soriano <vicentemline@gmail.com>
2024-09-06 12:24:35 +0200

    Add HealtCheckModule to AppModule and log Health Check configuration

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
 	const logger = new Logger('Main-Gateway');
 
 	const app = await NestFactory.create(AppModule);
-	app.setGlobalPrefix('api');
+	app.setGlobalPrefix('api', {
+        exclude: [{
+            path: '',
+            method: RequestMethod.GET
+        }]
+    });
 
 	app.useGlobalPipes(
 		new ValidationPipe({
@@ -19,7 +24,7 @@ async function bootstrap() {
 	app.useGlobalFilters(new RpcCustomExceptionFilter());
 	await app.listen(envs.port);
 
-	console.log('Hola mundo - Tercer Cambio');
+	logger.log('Health Check configured');
 
 	logger.log(`Gateway running on port ${envs.port}`);
 }
